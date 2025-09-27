4417202a4be0bfeabe4399523a697a45b262c39f
conrad93 <nunes.conrad93@gmail.com>
2025-09-28 01:27:14 +0530

    angular init

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SoccerComponent } from './soccer/soccer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'soccer',
+        component: SoccerComponent,
+    }
+];
---
+++ frontend/src/app/soccer/soccer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soccer',
+  standalone: true,
+  imports: [],
+  templateUrl: './soccer.component.html',
+  styleUrl: './soccer.component.css'
+})
+export class SoccerComponent {
+
+}
